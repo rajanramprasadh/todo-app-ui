d08e77c3cf32b6be18cb2875098280deebfcf890
Varadharajan <ram.pra.v@dxc.com>
2019-09-25 09:53:59 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoEditComponent
   ],
   imports: [
     BrowserModule
